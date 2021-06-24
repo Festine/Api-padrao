15b2f52ca8a7a12c1a123d32db70d91aa2061b58
Victor <victor_festine@hotmail.com>
2021-06-24 14:57:25 -0300

    Alterações nas rotas

--- api/controllers/controllerDefault.js
+++ api/controllers/controllerDefault.js
@@ -1,6 +1,7 @@
 const {json} = require("body-parser");
 const dateAction = require("../controllers/horaAction");
-const getDefault1 = require("../data/getDefault.json");
+const responseGet = require("../data/getDefault.json");
+const responsePost = require("../data/postDefault.json");
 
 module.exports = () => {
   const controller = {};
@@ -10,10 +11,22 @@ module.exports = () => {
       res.status(404).end();
     }
     else{
-      res.status(200).json(getDefault1);
+      res.status(200).json(responseGet);
     }
     console.log("Requisição GET:", req.body);
-    console.log("Resposta para o cliente:",getDefault1);
+    console.log("Resposta para o cliente:",responseGet);
+    dateAction();
+  };
+
+  controller.postDefault = (req, res) =>{
+    if (!req.body){
+      res.status(404).end();
+    }
+    else{
+      res.status(201).json(responsePost);
+    }
+    console.log("Requisição POST:", req.body);
+    console.log("Resposta para o cliente:",responsePost);
     dateAction();
   };
 
--- api/routes/rotasDefault.js
+++ api/routes/rotasDefault.js
@@ -2,6 +2,6 @@ module.exports = app => {
   const controller = require('../controllers/controllerDefault')();
 
   app.get('/get/', controller.getDefault);
-  app.post('/post/', controller.getDefault);
+  app.post('/post/', controller.postDefault);
 
 }
